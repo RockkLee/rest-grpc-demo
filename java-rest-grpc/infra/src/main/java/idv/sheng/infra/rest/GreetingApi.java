037efe030fd5fa1e0d956b5c41bbaa33aef05beb
RockkLee <sam880103@gmail.com>
2024-04-22 02:12:35 +0800

    Place SpringBootApplication int tje new module named "app"

--- java-rest-grpc/infra/src/main/java/idv/sheng/infra/rest/GreetingApi.java
+++ java-rest-grpc/infra/src/main/java/idv/sheng/infra/rest/GreetingApi.java
@@ -1,4 +1,4 @@
-package idv.sheng.rest;
+package idv.sheng.infra.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- java-rest-grpc/infra/src/main/java/idv/sheng/infra/rest/SpringDocConfig.java
+++ java-rest-grpc/infra/src/main/java/idv/sheng/infra/rest/SpringDocConfig.java
@@ -1,4 +1,4 @@
-package idv.sheng.rest;
+package idv.sheng.infra.rest;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
