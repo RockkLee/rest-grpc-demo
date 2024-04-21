21674f891b94e29554b25bc430645628e1bee509
RockkLee <sam880103@gmail.com>
2024-04-22 00:12:22 +0800

    Create project "java-rest-grpc" with DDD architecture style.

---
+++ java-rest-grpc/infra/src/main/java/idv/sheng/rest/GreetingApi.java
@@ -0,0 +1,17 @@
+package idv.sheng.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingApi {
+    @GetMapping("/hello/python")
+    public String helloPython() {
+        return "Todo: The grpc-client feature is not ready yet.";
+    }
+
+    @GetMapping("/hello/golang")
+    public String helloGo() {
+        return "Todo: The grpc-client feature is not ready yet.";
+    }
+}
---
+++ java-rest-grpc/infra/src/main/java/idv/sheng/rest/SpringDocConfig.java
@@ -0,0 +1,26 @@
+package idv.sheng.rest;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI baseOpenAPI() {
+        return new OpenAPI().info(
+                new Info()
+                .title("Rest & Grpc demo")
+                .description("SpringBoot 3.x application")
+                .version("v0.0.1")
+                .license(new License().name("Your License").url("http://springdoc.org"))
+                .contact(new Contact().name("Sheng Lee").email("aaa@aaa.com").url("https://github.com/RockkLee"))
+        );
+    }
+}
